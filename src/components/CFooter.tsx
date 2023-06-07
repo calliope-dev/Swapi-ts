eaf096a1a051eb3b7977e3c86f10aad744eb6050
Limky <antonionetosg3@gmail.com>
2023-06-07 17:50:17 -0300

    feat: implementa footer.tsx

--- src/components/CFooter.tsx
+++ src/components/CFooter.tsx
@@ -1,7 +1,10 @@
 const Footer = () => {
   return (
-    <footer>
-      Footer
+    <footer className="flex bg-zinc-900 text-white p-2">
+      <nav className="flex flex-col underline">
+        <a href="https://github.com/Lymky/z-project" target="_blank">Support</a>
+        <a href="https://github.com/Lymky" target="_blank">GitHub</a>
+      </nav>
     </footer>
   );
 };
