f3f10d96fe8acb3d062a17fb4ed8f1b065ac508f
Limky <antonionetosg3@gmail.com>
2023-06-07 15:16:49 -0300

    feat: redireciona o usuário após o registro

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -18,39 +18,67 @@ const registerSchema = z.object({
     .min(6, 'Precisa ser no mínimo 6 caracteres'),
 });
 
+type registerFormData = z.infer<typeof registerSchema>;
+
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<registerFormData>({
+    resolver: zodResolver(registerSchema)
+  });
+
+  const onClickFunction = (data: object) => {
+    console.log(data);
+  };
+
   return (
     <main
       className=" h-screen bg-zinc-900 flex items-center justify-center"
     >
       <form
         className="flex flex-col gap-4 w-xs text-white"
+        onSubmit={handleSubmit(onClickFunction)}
       >
         <div
           className="flex flex-col gap-1"
         >
           <label htmlFor="name">Nome</label>
           <input
+            className="text-black"
             type="text"
+            {...register('name')}
           />
+          {errors.name && <span>{errors.name.message}</span>}
         </div>
         <div
           className="flex flex-col gap-1"
         >
           <label htmlFor="email">Email</label>
           <input
+            className="text-black"
             type="text"
+            {...register('email')}
           />
+          {errors.email && <span>{errors.email.message}</span>}
         </div>
         <div
           className="flex flex-col gap-1"
         >
-          <label htmlFor="password">Password</label>
+          <label htmlFor="password">Senha</label>
           <input
+            className="text-black"
             type="password"
+            {...register('password')}
           />
+          {errors.password && <span>{errors.password.message}</span>}
         </div>
-        <button>Registrar</button>
+        <button
+          type="submit"
+        >
+          Registrar
+        </button>
       </form>
     </main>
   );
