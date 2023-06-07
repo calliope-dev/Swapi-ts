6a1f0d14943b13f168492a56fbbe4a8c51a3d63b
Limky <antonionetosg3@gmail.com>
2023-06-07 12:04:21 -0300

    feat: implementação do formulário

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.js';
+import App from './App.tsx';
 
 // Styles
-import './styles/Global.css'
+import './styles/Global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,36 @@
 const Login = () => {
   return (
-    <span>Hello There!</span>
+    <main>
+      <form>
+        <div>
+          <label htmlFor="name-input">Nome</label>
+          <input
+            type="text"
+            name="name-input"
+          />
+        </div>
+        <div>
+          <label htmlFor="email-input">Email</label>
+          <input
+            type="text"
+            name="email-input"
+          />
+        </div>
+        <div>
+          <label htmlFor="password-input">Senha</label>
+          <input
+            type="password"
+            name="password-input"
+          />
+        </div>
+        <button
+          type="button"
+          name="confirm-button"
+        >
+          Confirmar
+        </button>
+      </form>
+    </main>
   )
 }
 
