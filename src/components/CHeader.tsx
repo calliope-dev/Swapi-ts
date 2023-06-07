a6e473ac1dcd602c38ccc26a2a95c226fd7daa14
Limky <antonionetosg3@gmail.com>
2023-06-07 17:01:35 -0300

    feat: implementação da home.tsx e header.tsx

---
+++ src/components/CHeader.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+const Header = ({ userName }: { userName: string }) => {
+  return (
+    <header
+      className="flex bg-zinc-900 text-white p-2"
+    > 
+      <div className="flex align-center justify-center gap-4 w-32">
+        <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="icon" width="30px" />
+        <span>{userName}</span>
+      </div>
+      <div className="flex align-center justify-end gap-4 w-full mr-12">
+        <nav>
+          <Link to='/login'>
+            <img src="https://cdn-icons-png.flaticon.com/512/1053/1053210.png" alt="logout" width="30px" />
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import { CFooter, CHeader } from "../components/Index";
+
 const Home = () => {
-    return (
-        <h1>Hello There!</h1>
-    );
+  const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    try {
+      const { name } = JSON.parse(localStorage.getItem('sucess_login') || '{}');
+      setUserName(name);
+    } catch (errors) {
+      console.log(errors);
+    }
+  }, []);
+
+  return (
+    <>
+      <CHeader userName={userName} />
+      <main
+        className="h-screen"
+      >
+        Homepage
+      </main>
+      <CFooter />
+    </>
+  );
 };
 
 export default Home;
