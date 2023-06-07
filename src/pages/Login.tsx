d8a199a9873ae1d9da3802f339c9fdf68b1e405d
Limky <antonionetosg3@gmail.com>
2023-06-07 13:18:24 -0300

    feat: implementação da validação e estilização

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,36 +1,83 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from "zod";
+
+const loginSchema = z.object({
+  name: z.string(),
+  email: z.string()
+    .nonempty('Email é obrigatório')
+    .email("Email inválido"),
+  password: z.string()
+    .min(6, 'Precisa ser no mínimo 6 caracteres'),
+})
+
+type loginFormData = z.infer<typeof loginSchema>
+
 const Login = () => {
+  const [submitData, setData] = useState('');
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<loginFormData>({
+    resolver: zodResolver(loginSchema)
+  });
+
+  const onClickFunction = (data: any) => {
+    setData(JSON.stringify(data, null, 2))
+  };
+
   return (
-    <main>
-      <form>
-        <div>
-          <label htmlFor="name-input">Nome</label>
+    <main
+      className=" h-screen bg-zinc-900 flex items-center justify-center"
+    >
+      <form
+        className="flex flex-col gap-4 w-xs text-white"
+        onSubmit={handleSubmit(onClickFunction)}
+      >
+        <div
+          className="flex flex-col gap-1"
+        >
+          <label htmlFor="name">Nome</label>
+          <input
+            className="text-black"
+            type="text"
+            {...register('name')}
+          />
+          {errors.name && <span>{errors.name.message}</span>}
+        </div>
+        <div
+          className="flex flex-col gap-1"
+        >
+          <label htmlFor="email">Email</label>
           <input
             type="text"
-            name="name-input"
+            className="text-black"
+            {...register('email')}
           />
+          {errors.email && <span>{errors.email.message}</span>}
         </div>
-        <div>
-          <label htmlFor="email-input">Email</label>
-          <input
-            type="text"
-            name="email-input"
-          />
-        </div>
-        <div>
-          <label htmlFor="password-input">Senha</label>
+        <div
+          className="flex flex-col gap-1"
+        >
+          <label htmlFor="password">Senha</label>
           <input
             type="password"
-            name="password-input"
+            className="text-black"
+            {...register('password')}
           />
         </div>
         <button
-          type="button"
+          type="submit"
           name="confirm-button"
         >
           Confirmar
         </button>
       </form>
-    </main>
+      <pre>{submitData}</pre>
+    </main >
   )
 }
 
